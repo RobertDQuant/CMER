27b7de0d6ec23e58b75a677eadc16f913e2c09db
emel lopez <emlopez88@gmail.com>
2017-05-25 10:18:19 -0400

    Nav & Footer Components done & added css

--- cmer/src/App.js
+++ cmer/src/App.js
@@ -1,13 +1,23 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Nav from './components/Navigation';
+
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom'
+import Footer from './components/Footer';
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Nav />
-        <Footer />
+        
+      <Nav/>
+      <Footer />
       </div>
     );
   }
