fce7cd39549ed14259d525bffde0d9fd83f325a0
Madeline <madelinenelsonfolkersen@gmail.com>
2017-05-25 10:58:35 -0400

    added react router dom npm

--- cmer/src/App.js
+++ cmer/src/App.js
@@ -14,8 +14,7 @@ import Footer from './components/Footer';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        
+      <div className="App"> 
       <Nav/>
       <Footer />
       </div>
